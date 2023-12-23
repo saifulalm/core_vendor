53c231e6cd87dc411cc3045898622af2a9fe4a84
Saiful <ipoela@gmail.com>
2023-12-23 11:00:55 +0700

    Updated vendor Masa

--- controllers/Masa.js
+++ controllers/Masa.js
@@ -73,7 +73,7 @@ controller.index = async (req, res) =>{
 
 controller.callback = async (req, res) =>{
 
-  logToLogFile(`Callback ${vendor} : ${JSON.stringify(req.query)}`, customLogPath);
+  logToLogFile(`Callback ${vendor} : ${req.query}`, customLogPath);
   try {
     const { serverid, clientid, kp, msisdn, sn, msg, statuscode } = req.query;
 
